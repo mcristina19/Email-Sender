74b34f72eb59a36d1e7f5346990e8e55f260645f
Megan <112894384+mcristina19@users.noreply.github.com>
2023-03-28 12:23:25 -0400

    Add files via upload

---
+++ Email_Sender.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Megan'
+email['to'] = 'dummy_email@yahoo.com'
+email['subject'] = 'Congragulations!'
+
+email.set_content('I am a Python Master')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login('fake_email@gmail.com', 'bbnmgvksdnoinsdof')
+	smtp.send_message(email)
+	print('all good boss!')
